64c999086a19feebc0f4cc64ae30880705545de5
shinespark <shinespark64@gmail.com>
2017-10-26 20:16:47 +0900

    途中コミット

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,29 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by metroid on 2017/10/26.
+//  Copyright © 2017 shinespark. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
